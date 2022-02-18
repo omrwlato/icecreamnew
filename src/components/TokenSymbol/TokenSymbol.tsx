05f328e8a58017ef209e2f3b0430db5c9919172a
Ron <icecreamfinancial@protonmail.com>
2022-02-17 22:57:10 -0500

    update

--- src/components/TokenSymbol/TokenSymbol.tsx
+++ src/components/TokenSymbol/TokenSymbol.tsx
@@ -5,19 +5,12 @@ import tombLogo from '../../assets/img/3OMB.svg';
 import tShareLogo from '../../assets/img/3SHARES.svg';
 import tombLogoPNG from '../../assets/img/3OMB.png';
 import tShareLogoPNG from '../../assets/img/3SHARES.png';
-import tBondLogo from '../../assets/img/3BOND-01.png';
-
+import tBondLogo from '../../assets/img/3BOND.png';
 import tombFtmLpLogo from '../../assets/img/tomb_ftm_lp.png';
 import tshareFtmLpLogo from '../../assets/img/tshare_ftm_lp.png';
-
 import wftmLogo from '../../assets/img/fantom-ftm-logo.png';
-import booLogo from '../../assets/img/spooky.png';
-import belugaLogo from '../../assets/img/BELUGA.png';
 import twoshareLogo from '../../assets/img/t_2SHARE-01.png';
 import twoombLogo from '../../assets/img/t_2OMB-01.png';
-import zooLogo from '../../assets/img/zoo_logo.svg';
-import shibaLogo from '../../assets/img/shiba_logo.svg';
-import bifiLogo from '../../assets/img/COW.svg';
 import mimLogo from '../../assets/img/mimlogopng.png';
 import bloomLogo from '../../assets/img/BLOOM.jpg';
 import TwoombLPLogo from '../../assets/img/2OMB-WFTM.png';
@@ -36,24 +29,19 @@ const logosBySymbol: { [title: string]: string } = {
   TSHARE: tShareLogo,
   TBOND: tBondLogo,
   WFTM: wftmLogo,
-  BOO: booLogo,
-  SHIBA: shibaLogo,
-  ZOO: zooLogo,
-  BELUGA: belugaLogo,
-  BIFI: bifiLogo,
   MIM: mimLogo,
   BLOOM: bloomLogo,
-  '2OMB-WFTM LP': TwoombLPLogo,
-  '2SHARES-WFTM LP': TwosharesLPLogo,
-  '2OMB-2SHARES LP': TwoombTwosharesLPLogo,
+  'CREAM-AVAX LP': TwoombLPLogo,
+  'CSHARE-AVAX LP': TwosharesLPLogo,
+  'CREAM-CSHARE LP': TwoombTwosharesLPLogo,
 
-  '3OMB-WFTM LP': ThreeombLPLogo,
-  '3SHARES-WFTM LP': ThreesharesLPLogo,
+  'FUDGE-DAI LP': ThreeombLPLogo,
+  'STRAW-DAI LP': ThreesharesLPLogo,
 
 
-  'wFTM': wftmLogo,
-  '2OMB': twoombLogo,
-  '2SHARES': twoshareLogo,
+  'DAI': wftmLogo,
+  'CREAM': twoombLogo,
+  'CSHARE': twoshareLogo,
   'TOMB-FTM-LP': tombFtmLpLogo,
   'TSHARE-FTM-LP': tshareFtmLpLogo,
 };
