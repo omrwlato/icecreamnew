2ebc97056b5691839554843c3eec964792559791
Ron <icecreamfinancial@protonmail.com>
2022-02-17 23:58:55 -0500

    update

--- src/components/TokenSymbol/TokenSymbol.tsx
+++ src/components/TokenSymbol/TokenSymbol.tsx
@@ -6,17 +6,15 @@ import tShareLogo from '../../assets/img/3SHARES.svg';
 import tombLogoPNG from '../../assets/img/3OMB.png';
 import tShareLogoPNG from '../../assets/img/3SHARES.png';
 import tBondLogo from '../../assets/img/3BOND.png';
+import wftmLogo from '../../assets/img/Dai.png';
+import twoshareLogo from '../../assets/img/cshare.png';
+import twoombLogo from '../../assets/img/cream.png';
+import TwoombLPLogo from '../../assets/img/cream-wavax.png';
+import TwosharesLPLogo from '../../assets/img/cshare-avax.png';
+import TwoombTwosharesLPLogo from '../../assets/img/creamcshare.png';
+
 import tombFtmLpLogo from '../../assets/img/tomb_ftm_lp.png';
 import tshareFtmLpLogo from '../../assets/img/tshare_ftm_lp.png';
-import wftmLogo from '../../assets/img/fantom-ftm-logo.png';
-import twoshareLogo from '../../assets/img/t_2SHARE-01.png';
-import twoombLogo from '../../assets/img/t_2OMB-01.png';
-import mimLogo from '../../assets/img/mimlogopng.png';
-import bloomLogo from '../../assets/img/BLOOM.jpg';
-import TwoombLPLogo from '../../assets/img/2OMB-WFTM.png';
-import TwosharesLPLogo from '../../assets/img/2SHARES-WFTM.png';
-import TwoombTwosharesLPLogo from '../../assets/img/2OMB-2SHARES.png';
-
 import ThreeombLPLogo from '../../assets/img/3OMB-WFTM.png';
 import ThreesharesLPLogo from '../../assets/img/3SHARES-WFTM.png';
 
@@ -29,19 +27,16 @@ const logosBySymbol: { [title: string]: string } = {
   TSHARE: tShareLogo,
   TBOND: tBondLogo,
   WFTM: wftmLogo,
-  MIM: mimLogo,
-  BLOOM: bloomLogo,
   'CREAM-AVAX LP': TwoombLPLogo,
   'CSHARE-AVAX LP': TwosharesLPLogo,
   'CREAM-CSHARE LP': TwoombTwosharesLPLogo,
+  'CREAM': twoombLogo,
+  'CSHARE': twoshareLogo,
+
 
   'FUDGE-DAI LP': ThreeombLPLogo,
   'STRAW-DAI LP': ThreesharesLPLogo,
-
-
   'DAI': wftmLogo,
-  'CREAM': twoombLogo,
-  'CSHARE': twoshareLogo,
   'TOMB-FTM-LP': tombFtmLpLogo,
   'TSHARE-FTM-LP': tshareFtmLpLogo,
 };
