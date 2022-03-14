f4e3d3f959caa83a73b30908ba5ec3c3e304d518
mrdefidev <jahaveve@hotmail.com>
2022-03-13 21:22:11 -0500

    Homepage update

--- src/views/Cemetery/CemeteryCard.js
+++ src/views/Cemetery/CemeteryCard.js
@@ -30,8 +30,7 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 async function watchAssetInMetamask(asset) {
   const { ethereum } = window;
 
-
-  const symbolName = asset.depositTokenName.substring(0, 11)
+  const symbolName = asset.depositTokenName.substring(0, 11);
   if (ethereum && ethereum.chainId === '0xa86a') {
     await ethereum.request({
       method: 'wallet_watchAsset',
@@ -55,9 +54,8 @@ const useStyles = makeStyles(() => ({
       textDecoration: 'none',
       color: '#2596be !important',
     },
-  }
-}))
-
+  },
+}));
 
 const CemeteryCard = ({ bank }) => {
   const [show, setShow] = useState(false);
@@ -71,6 +69,7 @@ const CemeteryCard = ({ bank }) => {
   const tShareStats = useShareStats();
   const tokenStats = bank.earnTokenName === 'CSHARE' ? tShareStats : bombStats;
   const stakedBalance = useStakedBalance(bank.contract, bank.poolId);
+  console.log(bank, 'bank');
   const stakedTokenPriceInDollars = useStakedTokenPriceInDollars(bank.depositTokenName, bank.depositToken);
   const tokenPriceInDollars = useMemo(
     () => (tokenStats ? Number(tokenStats.priceInDollars).toFixed(2) : null),
@@ -110,48 +109,72 @@ const CemeteryCard = ({ bank }) => {
   );
 
   return (
-    < Grid item xs={12}>
-      <Card >
+    <Grid item xs={12}>
+      <Card>
         <CardContent style={{ position: 'relative', backgroundColor: 'white' }}>
-          <Grid justifyContent="space-between" style={{ display: 'flex', flexDirection: "row" }}>
+          <Grid justifyContent="space-between" style={{ display: 'flex', flexDirection: 'row' }}>
             <Grid alignItems="flex-start" justifyContent="space-between" style={{ display: 'flex' }}>
               <Grid style={{ padding: '10px' }}>
+                {console.log(bank.depositTokenName, 'sdada')}
                 <TokenSymbol symbol={bank.depositTokenName} />
               </Grid>
-              <Grid style={{ padding: '10px', display: 'flex', flexDirection: "row", alignContent: "center" }} mt={2} mb={2}>
+              <Grid
+                style={{ padding: '10px', display: 'flex', flexDirection: 'row', alignContent: 'center' }}
+                mt={2}
+                mb={2}
+              >
                 <Grid>
-                  <Link style={{ textDecoration: 'none', color: '#2596be' }} to={{ pathname: `https://snowtrace.io/address/${bank.address}` }} target="_blank">
+                  <Link
+                    style={{ textDecoration: 'none', color: '#2596be' }}
+                    to={{ pathname: `https://snowtrace.io/address/${bank.address}` }}
+                    target="_blank"
+                  >
                     <h2 style={{ color: 'black', padding: '10px' }}>{bank.depositTokenName}</h2>
                   </Link>
                 </Grid>
                 <Grid style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                  <Link style={{ textDecoration: 'none', color: '#2596be' }} to={{ pathname: `https://snowtrace.io/address/${bank.address}` }} target="_blank">
+                  <Link
+                    style={{ textDecoration: 'none', color: '#2596be' }}
+                    to={{ pathname: `https://snowtrace.io/address/${bank.address}` }}
+                    target="_blank"
+                  >
                     <InputIcon style={{ color: 'black' }} />
                   </Link>
                 </Grid>
               </Grid>
             </Grid>
 
-            <Grid alignItems="center" justifyContent="center" style={{ display: 'flex', flexDirection: "row", width: '60%' }}>
-
-              <Grid container item alignItems="center" justifyContent="center" style={{ display: 'flex', flexDirection: "column" }} >
+            <Grid
+              alignItems="center"
+              justifyContent="center"
+              style={{ display: 'flex', flexDirection: 'row', width: '60%' }}
+            >
+              <Grid
+                container
+                item
+                alignItems="center"
+                justifyContent="center"
+                style={{ display: 'flex', flexDirection: 'column' }}
+              >
                 <Grid item alignItems="center" justifyContent="center" style={{ padding: '10px' }}>
                   <h3 style={{ color: 'black' }}>{`Staked`}</h3>
                 </Grid>
                 <Grid item alignItems="center" justifyContent="center" style={{ padding: '10px' }}>
                   {approveStatus !== ApprovalState.APPROVED ? (
-                    <h2 style={{ color: 'black' }}>
-                      {`≈ $0`}
-                    </h2>
+                    <h2 style={{ color: 'black' }}>{`≈ $0`}</h2>
                   ) : (
-                    <h2 style={{ color: 'black' }}>
-                      {`≈ $${stakedInDollars}`}
-                    </h2>
+                    <h2 style={{ color: 'black' }}>{`≈ $${stakedInDollars}`}</h2>
                   )}
                 </Grid>
               </Grid>
 
-              <Grid container item alignItems="center" justifyContent="center" style={{ display: 'flex', flexDirection: "column" }} >
+              <Grid
+                container
+                item
+                alignItems="center"
+                justifyContent="center"
+                style={{ display: 'flex', flexDirection: 'column' }}
+              >
                 <Grid item alignItems="center" justifyContent="center" style={{ padding: '10px' }}>
                   <h3 style={{ color: 'black' }}>{`TVL`}</h3>
                 </Grid>
@@ -160,7 +183,13 @@ const CemeteryCard = ({ bank }) => {
                 </Grid>
               </Grid>
 
-              <Grid container item alignItems="center" justifyContent="center" style={{ display: 'flex', flexDirection: "column" }} >
+              <Grid
+                container
+                item
+                alignItems="center"
+                justifyContent="center"
+                style={{ display: 'flex', flexDirection: 'column' }}
+              >
                 <Grid item alignItems="center" justifyContent="center" style={{ padding: '10px' }}>
                   <h3 style={{ color: 'black' }}>{`APR`}</h3>
                 </Grid>
@@ -169,7 +198,13 @@ const CemeteryCard = ({ bank }) => {
                 </Grid>
               </Grid>
 
-              <Grid container item alignItems="center" justifyContent="center" style={{ display: 'flex', flexDirection: "column" }} >
+              <Grid
+                container
+                item
+                alignItems="center"
+                justifyContent="center"
+                style={{ display: 'flex', flexDirection: 'column' }}
+              >
                 <Button
                   color="primary"
                   variant="contained"
@@ -182,47 +217,59 @@ const CemeteryCard = ({ bank }) => {
                 </Button>
               </Grid>
 
-              <Grid container item alignItems="center" justifyContent="center" style={{ display: 'flex', flexDirection: "column" }} >
-                <Button className={classes.detailsButton}
+              <Grid
+                container
+                item
+                alignItems="center"
+                justifyContent="center"
+                style={{ display: 'flex', flexDirection: 'column' }}
+              >
+                <Button
+                  className={classes.detailsButton}
                   style={{ borderRadius: '10px', textDecoration: 'none' }}
-                  onClick={() => setShow(prev => !prev)}>
-                  {show ? <ArrowDropUpTwoTone sx={{ fontSize: 40 }} style={{ color: 'black' }} /> : <ArrowDropDownTwoTone sx={{ fontSize: 40 }} style={{ color: 'black' }} />}</Button>
+                  onClick={() => setShow((prev) => !prev)}
+                >
+                  {show ? (
+                    <ArrowDropUpTwoTone sx={{ fontSize: 40 }} style={{ color: 'black' }} />
+                  ) : (
+                    <ArrowDropDownTwoTone sx={{ fontSize: 40 }} style={{ color: 'black' }} />
+                  )}
+                </Button>
               </Grid>
-
-
             </Grid>
           </Grid>
           <div>
-            {show ?
+            {show ? (
               <div>
-                <hr style={{
-                  color: '#D3D3D3',
-                  backgroundColor: '#D3D3D3',
-                  height: 1
-                }} />
+                <hr
+                  style={{
+                    color: '#D3D3D3',
+                    backgroundColor: '#D3D3D3',
+                    height: 1,
+                  }}
+                />
 
-                <Grid justifyContent="center" style={{ display: 'flex', flexDirection: "row" }}>
-
+                <Grid justifyContent="center" style={{ display: 'flex', flexDirection: 'row' }}>
                   <Grid>
                     <Grid justifyContent="center">
                       <h3 style={{ color: 'black' }}>Deposit</h3>
                     </Grid>
-                    <hr style={{
-                      color: '#D3D3D3',
-                      backgroundColor: '#D3D3D3',
-                      height: 1
-                    }} />
-
+                    <hr
+                      style={{
+                        color: '#D3D3D3',
+                        backgroundColor: '#D3D3D3',
+                        height: 1,
+                      }}
+                    />
                   </Grid>
 
-                  <Grid>
-
-
-                  </Grid>
-
+                  <Grid></Grid>
                 </Grid>
 
-                <Grid justifyContent="space-between" style={{ display: 'flex', marginBottom: '5px', marginTop: '10px' }}>
+                <Grid
+                  justifyContent="space-between"
+                  style={{ display: 'flex', marginBottom: '5px', marginTop: '10px' }}
+                >
                   <h3 style={{ marginBottom: '10px', color: 'black' }}>APR:</h3>
                   <h4 style={{ color: 'black' }}>{statsOnPool?.dailyAPR}%</h4>
                 </Grid>
@@ -231,17 +278,28 @@ const CemeteryCard = ({ bank }) => {
                   <h4 style={{ color: 'black' }}>{bank.earnTokenName}</h4>
                 </Grid>
                 <Grid>
-                  <h3 style={{ color: 'black', marginBottom: '10px', marginTop: '15px' }}>{`${bank.earnTokenName} EARNED`}</h3>
+                  <h3
+                    style={{ color: 'black', marginBottom: '10px', marginTop: '15px' }}
+                  >{`${bank.earnTokenName} EARNED`}</h3>
                 </Grid>
-                <Grid alignItems="flex-start" justifyContent="space-between" style={{ display: 'flex', marginBottom: '10px' }}>
+                <Grid
+                  alignItems="flex-start"
+                  justifyContent="space-between"
+                  style={{ display: 'flex', marginBottom: '10px' }}
+                >
                   <Grid>
                     <h2 style={{ color: 'black' }}>{getDisplayBalance(earnings)}</h2>
-                    <h5 style={{ color: 'grey', marginTop: '5px', marginBottom: '15px', fontWeight: 'normal' }}>{`≈ $${earnedInDollars}`}</h5>
+                    <h5
+                      style={{ color: 'grey', marginTop: '5px', marginBottom: '15px', fontWeight: 'normal' }}
+                    >{`≈ $${earnedInDollars}`}</h5>
                   </Grid>
-                  <Button onClick={onReward} color="primary"
+                  <Button
+                    onClick={onReward}
+                    color="primary"
                     variant="contained"
                     style={{ borderRadius: '10px' }}
-                    className="shinyButtonSecondary">
+                    className="shinyButtonSecondary"
+                  >
                     Collect
                   </Button>
                 </Grid>
@@ -250,7 +308,6 @@ const CemeteryCard = ({ bank }) => {
                     <h3 style={{ marginBottom: '10px', color: 'black' }}>{`${bank.depositTokenName} Staked`}</h3>
                   </Grid>
                   <Grid alignItems="center">
-
                     <span>
                       {approveStatus !== ApprovalState.APPROVED ? (
                         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -266,7 +323,6 @@ const CemeteryCard = ({ bank }) => {
                             style={{
                               marginBottom: '5px',
                               borderRadius: '10px',
-
                             }}
                             className="shinyButtonSecondary"
                           >
@@ -274,9 +330,10 @@ const CemeteryCard = ({ bank }) => {
                           </Button>
                         </div>
                       ) : (
-                        <Grid container justifyContent='space-between' alignItems='flex-start' flex spacing={2}>
+                        <Grid container justifyContent="space-between" alignItems="flex-start" flex spacing={2}>
                           <Grid item>
                             <h2 style={{ color: 'black' }}>
+                              {console.log(bank.depositToken.decimal, stakedBalance)}
                               {getDisplayBalance(stakedBalance, bank.depositToken.decimal)}
                             </h2>
                             <h5 style={{ color: 'grey', fontWeight: 'normal', marginTop: '5px' }}>
@@ -286,19 +343,25 @@ const CemeteryCard = ({ bank }) => {
                           <Grid item>
                             <Grid container flex spacing={2}>
                               <Grid item>
-                                <Button onClick={onPresentWithdraw} color="primary"
+                                <Button
+                                  onClick={onPresentWithdraw}
+                                  color="primary"
                                   variant="contained"
                                   style={{ borderRadius: '10px' }}
-                                  className="shinyButtonSecondary">
+                                  className="shinyButtonSecondary"
+                                >
                                   -
                                 </Button>
                               </Grid>
-                              <Grid item >
-                                <Button color="primary"
+                              <Grid item>
+                                <Button
+                                  color="primary"
                                   variant="contained"
                                   style={{ borderRadius: '10px' }}
-                                  className="shinyButtonSecondary" disabled={bank.closedForStaking}
-                                  onClick={() => (bank.closedForStaking ? null : onPresentDeposit())}>
+                                  className="shinyButtonSecondary"
+                                  disabled={bank.closedForStaking}
+                                  onClick={() => (bank.closedForStaking ? null : onPresentDeposit())}
+                                >
                                   +
                                 </Button>
                               </Grid>
@@ -310,11 +373,13 @@ const CemeteryCard = ({ bank }) => {
                   </Grid>
                 </Grid>
               </div>
-              : <div />}
+            ) : (
+              <div />
+            )}
           </div>
         </CardContent>
-      </Card >
-    </Grid >
+      </Card>
+    </Grid>
   );
 };
 
